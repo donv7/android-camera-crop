c78554b56de46dc28035d47b519ac7259b05ddca
Don Virostek <don@rhumbix.com>
2016-01-16 16:12:40 -0800

    add a button to test camera crop stuff

--- app/src/main/java/donv7/android_easy_camera_crop/MainActivity.java
+++ app/src/main/java/donv7/android_easy_camera_crop/MainActivity.java
@@ -2,6 +2,9 @@ package donv7.android_easy_camera_crop;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.testButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.d("MainActivity", "testButton clicked!");
+
+                //Add camera stuff here
+            }
+        });
     }
 
 }
